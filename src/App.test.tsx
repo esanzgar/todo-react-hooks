3b22718e03aa987c99ee131abf84bcce838acc83
Eduardo Sanz García <esanzgar@gmail.com>
2019-07-03 17:07:24 +0100

    Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,50 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('TodoList', () => {
+  it('renders', () => {
+    shallow(<App />);
+  });
+
+  it('initally displays 3 todo items', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.todo')).toHaveLength(3);
+    expect(wrapper.find('.todo-is-completed')).toHaveLength(1);
+  });
+
+  it('mark todo as completed', () => {
+    const wrapper = shallow(<App />);
+    const todos = wrapper.find('.todo');
+    expect(todos).toHaveLength(3);
+    expect(wrapper.find('.todo-is-completed')).toHaveLength(1);
+    todos
+      .last()
+      .find('.checkbox')
+      .simulate('click');
+    expect(wrapper.find('.todo-is-completed')).toHaveLength(2);
+  });
+
+  it('add todo item', () => {
+    const wrapper = shallow(<App />);
+    const todos = wrapper.find('.todo');
+    expect(todos).toHaveLength(3);
+    expect(wrapper.find('.todo-is-completed')).toHaveLength(1);
+    todos
+      .last()
+      .find('input')
+      .simulate('keydown', {key: 'Enter'});
+    expect(wrapper.find('.todo-is-completed')).toHaveLength(1);
+    expect(wrapper.find('.todo')).toHaveLength(4);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
+import React, {useState, useRef, KeyboardEvent} from 'react';
+
+// import v5 from 'uuid/v5';
+
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [todos, setTodos] = useState([
+    {
+      content: 'Pickup dry cleaning',
+      isCompleted: true,
+    },
+    {
+      content: 'Get haircut',
+      isCompleted: false,
+    },
+    {
+      content: 'Build a todo app in React',
+      isCompleted: false,
+    },
+  ]);
+
+  const handleChange = (value: string, index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].content = value;
+    setTodos(newTodos);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent, index: number) => {
+    if (event.key === 'Enter') {
+      createTodoAtIndex(index + 1);
+    }
+
+    if (event.key === 'Backspace' && todos[index].content === '') {
+      event.preventDefault();
+      deleteTodoAtIndex(index);
+    }
+  };
+
+  const createTodoAtIndex = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 0, {content: '', isCompleted: false});
+    setTodos(newTodos);
+    window.setTimeout(() => {
+      formRef.current && (formRef.current.elements[index] as HTMLInputElement).focus();
+    }, 0);
+  };
+
+  const deleteTodoAtIndex = (index: number) => {
+    const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+    setTodos(newTodos);
+  };
+
+  const toggleDone = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
+  const allTodos = todos.map((todo, index) => (
+    <div
+      className={`todo ${todo.isCompleted && 'todo-is-completed'}`}
+      key={index}>
+      <div className="checkbox" onClick={() => toggleDone(index)} />
+      <input
+        type="text"
+        value={todo.content}
+        onChange={event => handleChange(event.target.value, index)}
+        onKeyDown={event => handleKeyDown(event, index)}
+      />
+    </div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className="todo-list" ref={formRef}>
+        <ul>{allTodos}</ul>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
